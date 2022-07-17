81282eed263a48bf904488d1d00662d00c8e0247
tomonari-s <108882451+tomonari-s@users.noreply.github.com>
2022-07-17 09:27:53 +0000

    create機能  3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,11 +4,24 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
+  end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/top'
   end
 
   def show
   end
 
   def edit
+  end
+  
+  private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end

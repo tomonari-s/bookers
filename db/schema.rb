8f174c9cd0b80099dff024fc940429ebb7748d9f
tomonari-s <108882451+tomonari-s@users.noreply.github.com>
2022-07-17 07:28:46 +0000

    ファイルの設定 3-12

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_16_215221) do
+ActiveRecord::Schema.define(version: 2022_07_17_063407) do
 
-  create_table "lists", force: :cascade do |t|
+  create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", precision: 6, null: false

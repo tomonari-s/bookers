8b3c34e1bb0d5faa79c42e3f29c0c213401ac559
tomonari-s <108882451+tomonari-s@users.noreply.github.com>
2022-07-19 14:00:46 +0000

    destoryの設定 3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,9 +14,23 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def destroy
+    book = book.find(params[:id])  
+    book.destroy  
+    redirect_to '/books'   
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)  
   end
   
   private

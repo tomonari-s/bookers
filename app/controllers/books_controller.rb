13485c99eb18b406134d6e9573c13d648c907ce9
tomonari-s <108882451+tomonari-s@users.noreply.github.com>
2022-07-24 08:20:28 +0000

    [finish]3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,9 +8,14 @@ class BooksController < ApplicationController
   end
   
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to '/top'
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def show
@@ -18,19 +23,26 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @books = Book.all
     @book = Book.find(params[:id])
   end
   
   def destroy
-    book = Book.find(params[:id])  
-    book.destroy  
+    @book = Book.find(params[:id])  
+    @book.destroy  
     redirect_to '/books'   
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)  
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    if @book.save
+      flash[:update] = "Book was successfully updated."
+      redirect_to book_path(@book.id)  
+    else
+      @books = Book.all
+      render :edit
+    end
   end
   
   private
